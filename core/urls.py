81f82d7b37b0cfd53c6fae744d3912132d3a8990
he-jaramillo <HE.JARAMILLO@duocuc.cl>
2023-04-25 23:08:22 -0400

    pagias agregadas
    juegos
    accesorios

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import accesorios, home, juegos
 
 urlpatterns = [
+    # INICIO
     path("", home, name="home"),
+
+    # PAGINAS
+    path('paginas/juegos', juegos, name="juegos"),
+    path('paginas/accesorios', accesorios, name="accesorios"),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# HOME
+
 
 def home(request):
-    return render(request, 'core/home.html')
+    return render(request, 'core/paginas/home.html')
+
+# PAGINAS
+
+
+def juegos(request):
+    return render(request, 'core/paginas/juegos.html')
+
+
+def accesorios(request):
+    return render(request, 'core/paginas/accesorios.html')
