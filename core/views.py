add8d35d4c362f6db5ea834ec96842f2a71dbe91
jossaldias <jos.saldias@duocuc.cl>
2023-04-25 23:21:31 -0400

    api juegos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
@@ -12,7 +13,10 @@ def home(request):
 
 
 def juegos(request):
-    return render(request, 'core/paginas/juegos.html')
+    response = requests.get('https://api.rawg.io/api/games?key=b40d42ec4f374f75aa29ef424c698357')
+    games = response.json()
+    print(games)
+    return render(request, 'core/paginas/juegos.html', {"juegos": games})
 
 
 def accesorios(request):
