9c5c965f4781c68434b08733fe25ea52d2557001
jossaldias <jos.saldias@duocuc.cl>
2023-05-05 00:44:43 -0400

    api

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nombres = models.CharField(max_length=80)
+    apellidos = models.CharField(max_length=80)
+    correo = models.EmailField(blank=True, primary_key=True)
+    telefono = models.IntegerField(default=1000)
+    fecha_nac = models.DateField(auto_now_add=True)
+    domicilio = models.CharField(max_length=80)
+    comuna = models.CharField(max_length=80)
+    pwd = models.CharField(max_length=8)
+    pwd1 = models.CharField(max_length=8)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from cmath import log
+from telnetlib import STATUS
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password
+
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
+@csrf_exempt
+@api_view(['POST'])
+def crearCliente(request):
+    username = request.POST['nombres']
+    email = request.POST['email']
+    pwd = request.POST['pwd']
+
+    Cliente.objects.create(
+        nombres = username,
+        apellidos = request.POST['apellidos'],
+        correo = email,
+        contraseña = pwd,
+        fecha_nacimiento = request.POST['fec_nac'],
+        telefono = request.POST['telefono'],
+        domicilio = request.POST['domicilio'],
+        comuna = request.POST['comuna']
+    )
+    return Response('Se ha registrado con exito.')
