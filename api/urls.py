5a2ff34569beb1af68c1ec6e935500e02a012914
jossaldias <jos.saldias@duocuc.cl>
2023-05-05 00:55:23 -0400

    .

---
+++ api/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from .views import crearCliente
+
+urlpatterns = [
+    # path('obtenerProductos/', obtenerProductos),
+    # path('crearProducto/', crearProducto),
+    # path('editarProducto/<p_id>', editarProducto),
+    # path('eliminarProducto/<p_id>', eliminarProducto),
+    # path('buscarProducto/<nombre>', buscarProducto),
+    # path('obtenerProductosDescuento/', obtenerProductosDescuento),
+
+    # path('obtenerClientes/', obtenerClientes),
+    path('crearCliente/', crearCliente),
+    # path('editarCliente/<p_id>', editarCliente),
+    # path('eliminarCliente/<p_id>', eliminarCliente),
+
+    # path('registroAdmin/', crearAdmin),
+    # path('loginAdmin/', loginAdmin),
+]
