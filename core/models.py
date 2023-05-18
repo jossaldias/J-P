232ee22dd6e518dcfe3aa0aff6bcee5d7de25932
jossaldias <jos.saldias@duocuc.cl>
2023-05-18 19:11:30 -0400

    model producto

--- core/models.py
+++ core/models.py
@@ -99,3 +99,22 @@ class User(AbstractUser):
     telefono = models.BigIntegerField(null=True)
     fecha_nac = models.DateField(null=True)
     tipo_user = models.CharField(max_length=60, null=True, blank =True)
+
+
+class Producto(models.Model):
+   id_poducto = models.CharField(max_length = 255, unique = True, null = True , blank = True)
+   descripcion = models.CharField(max_length = 255, unique = True, null = True)
+   imagen = models.ImageField(upload_to = 'productos', null = True, blank = True)
+   costo = models.DecimalField(max_digits=13, decimal_places = 2, null = False)
+   cantidad = models.DecimalField(max_digits=13, decimal_places = 2, null = False)
+   created = models.DateTimeField(auto_now_add=True)
+   updated = models.DateTimeField(auto_now_add=True)
+
+   class Meta:
+        verbose_name = 'producto'
+        verbose_name_plural = 'productos'
+        order_with_respect_to = 'descripcion'
+
+   def __str__(self):
+       return self.descripcion
+   
